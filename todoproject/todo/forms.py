3a6f5d83df5c8847ec589b185114c5b94d41ef9f
shakil <shakilx1473@gmail.com>
2018-07-27 01:15:43 +0600

    can add and remove items. developed by using python and django framework

---
+++ todoproject/todo/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class TodoForm(forms.Form):
+    text = forms.CharField(max_length=40,
+                           widget=forms.TextInput(
+                               attrs={'class': 'form-control', 'placeholder': 'Enter todo eg Delete junk file',
+                                      'aria-label': 'Todo', 'aria-describedby': 'add-btn'}
+                           ))
